58035f01de3c66cd72e551200aa4d1d0fe30f5b9
Gg273 <2627828030@qq.com>
2021-10-12 22:33:34 +0800

    depth testing

--- OpenGLTutorial/shaders/depth_test.fs
+++ OpenGLTutorial/shaders/depth_test.fs
@@ -5,21 +5,11 @@ uniform sampler2D texture1;
 
 in vec2 TexCoords;
 
-float near = 0.1; 
-float far  = 100.0; 
-
-float LinearizeDepth(float depth) 
-{
-    float z = depth * 2.0 - 1.0; // back to NDC 
-    return (2.0 * near * far) / (far + near - z * (far - near));    
-}
-
 void main()
 {
 
-//    vec3 result = vec3(texture(texture1, TexCoords));
-    
-    float depth = LinearizeDepth(gl_FragCoord.z) / far;
-    FragColor = vec4(vec3(depth), 1.0);
+    vec3 result = vec3(texture(texture1, TexCoords));
+
+    FragColor = vec4(result, 1.0);
 }
 
